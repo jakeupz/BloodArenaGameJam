9ba4eef25222011e01229ef588db465cbe634091
bobohotrod <bobohotrod2001@hotmail.com>
2023-08-12 16:55:38 -0500

    Enemy HP & HP Display
    added simple health system for enemies

--- Scenes/Enemies/Bat.gd
+++ Scenes/Enemies/Bat.gd
@@ -1,18 +1,29 @@
 extends Area2D
 
-func _process(delta):
-	pass
-#end _process
+@export var max_health : int = 5
+@onready var health : int = max_health
 
+
+func _process(_delta):
+	# Update the bat debugging health
+	#end _process
 
 
 func _on_area_entered(area):
-	print ("ChainAttack hit bat!")
 	
 	# If the area I am intersecting with is a player attack, kill me!
 	if area.is_in_group("player_attacks"):
-		print ("Bat killed!")
-		queue_free()
-		#end if
+		
+		# If the area that collided is ChainAttack.tscn's ChainAttack(Area2D)
+		if area.name == "ChainAttack":
+			health -= area.attack_damage
+			#end if
+		
+		# If bat is out of health, kill it / free memory
+		if health <= 0:
+			print ("Bat killed!")
+			# Delete this instance
+			queue_free()
+			#end if
 	
 	#end _on_area_entered
