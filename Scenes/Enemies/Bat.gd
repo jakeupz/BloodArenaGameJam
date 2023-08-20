1907e097ae5343c794abc9f19616d717a7158895
bobohotrod <bobohotrod2001@hotmail.com>
2023-08-20 17:15:54 -0500

    Bat update
    Added animation and sfx for bat, and fixed the bat to be a CharacterBody2D

--- Scenes/Enemies/Bat.gd
+++ Scenes/Enemies/Bat.gd
@@ -1,29 +1,68 @@
-extends Area2D
+extends CharacterBody2D
 
 @export var max_health : int = 5
 @onready var health : int = max_health
 
+var bat_death_sound = preload("res://Scenes/Enemies/BatDeathSFX.tscn")
+
+
+
+
+func _ready():
+	# Play the fly animation
+	$Sprite2D/AnimationPlayer.play("fly")
+	# Pick a random part of the fly animation so not all bats flap at the same time
+	$Sprite2D/AnimationPlayer.seek(randf_range(0.0, 1))
+
+	
+	#end ready
+
 
 func _process(_delta):
 	# Update the bat debugging health
-	$Bat/BatHealthLabel.text = str(health)
+	$HealthLabel.text = str(health)
+
+	# Keep the health label upright
+	$HealthLabel.rotation = -rotation
+	
 	#end _process
 
 
 func _on_area_entered(area):
+
 	
 	# If the area I am intersecting with is a player attack, hurt me!
 	if area.is_in_group("player_attacks"):
-		
+
 		# If the area that collided is Player/ChainAttackAnchor/ChainAttack
 		if area.name == "ChainAttack":
 			health -= area.attack_damage
+			play_hurt_sfx()
+				#end if
 			#end if
-		
+
 		# If bat is out of health, kill it / free memory
 		if health <= 0:
-			# Delete this instance
-			queue_free()
+			die()
 			#end if
-	
+
 	#end _on_area_entered
+
+
+func play_hurt_sfx():
+	if not $HurtSFX.is_playing():
+		$HurtSFX.play()
+	#end play_hurt_sfx
+
+
+func die():
+	print ("die(()")
+	# Make a new instance of the bat death scene
+	# So that it can play until completion
+	var death_sfx = bat_death_sound.instantiate()
+	death_sfx.position = position
+	get_parent().add_child(death_sfx)
+	
+	# Delete this bat instance
+	queue_free()
+	#end die
---
+++ Scripts/SoundInstance.gd
@@ -0,0 +1,21 @@
+extends AudioStreamPlayer2D
+
+# General purpose script for playing a sound until completion and then deleting
+# itself.
+# This is used so that we can have overlapping sound effects or death animations
+# that do not get cut off abruptly.
+
+# Make sure to instantiate the appropriate SFX scene and also make sure that
+# all signals are set up correctly or this could cause memory leaks.
+
+func _ready():
+	print("SoundInstanced")
+	# Play the sound
+	play()
+	#end ready
+
+# When the sound is done playing, free the audio stream player from memory
+func _on_finished():
+	print("freed queue!")
+	queue_free()
+	#end _on_finished
